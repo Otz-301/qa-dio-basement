6cb70b01d603eb96b09c31653d2d69da282ef848
Otz <otniel.haan@gmail.com>
2025-01-22 05:06:12 -0300

    Readme para o projeto MainNumberUtils

---
+++ Project_MainNumberUtils/src/MainNumberUtils.java
@@ -0,0 +1,7 @@
+// MainNumberUtils.java
+public class MainNumberUtils {
+    // Método estático para verificar se um número é ímpar
+    public static boolean isOdd(int number) {
+        return number % 2 != 0; // Se o resto da divisão por 2 for diferente de 0, o número é ímpar
+    }
+}
