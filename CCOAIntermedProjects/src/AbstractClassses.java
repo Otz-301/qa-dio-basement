9f4b6c8dd0666048172bae387e9a6db6353d3b7e
Otz <147393614+Otz-301@users.noreply.github.com>
2025-01-23 05:01:13 -0300

    Adicionando Abstract Classes Project

---
+++ CCOAIntermedProjects/src/AbstractClassses.java
@@ -0,0 +1,42 @@
+// Objetivo: Criar uma classe abstrata Forma e
+// implementar suas subclasses Circulo e Retangulo.
+
+    abstract class Forma{
+    abstract double calcularArea();
+
+    }
+    class Circulo extends Forma{
+    double raio;
+            public Circulo(double raio){
+                trhis.raio = raio;
+            }
+            @Override
+            double calcularArea(){
+                return Math.pi * raio * raio;
+            }
+            class Retangulo extends Forma{
+                double largura;
+                double altura;
+
+                    public Retangulo(double largura, double altura){
+                            this.largura = largura;
+                            this.altura = altura;
+                    }
+                    @Override
+                        double calcularArea(){
+                            return largura * altura;
+                    }
+            }
+
+            public class Main
+                public static voi main(string[] args){
+                Forma circulo = new Circulo(5);
+                Forma retangulo = new Retangulo(4, 6);
+
+                    System.out.println("Area do circulo: " + circulo.calcularArea());
+                            // 78.54
+                    System.out.println("Area do retangulo: " + retangulo.calcularArea());
+                            // 24.0
+                }
+
+    }
